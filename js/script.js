8fd746affebf3e0a4d3d28d6f8b522e05cd4d1a2
sophielorenz <sophielorenz1992@gmail.com>
2017-02-22 15:43:23 -0600

    finished home page structure, added carousel, added mobile nav, started contact page structure

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.mobile-nav-btn').click(function(e){
+		$('.mobile-nav').slideToggle();
+		// this prevents the browser from doing the default link action
+		e.preventDefault();
+	});
+
+
+	var $window = $(window);
+	  $window.on('resize', function (){
+        if ($window.width() > 800)
+        {
+            $('.mobile-nav').hide();
+        }
+    });
+});
